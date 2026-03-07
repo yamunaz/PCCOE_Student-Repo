013dbe217671fc8a90f0f35e6b5d2aff14d1a8d2
pratham1045wagh <pratham1045wagh@gmail.com>
2026-03-07 23:45:54 +0530

    Delete agent directory

--- agent/actions.py
+++
@@ -1,6 +0,0 @@
-ACTIONS = [
-    "dispatch_ambulance",
-    "dispatch_fire",
-    "redirect_traffic",
-    "wait"
-]
--- agent/reward.py
+++
@@ -1,7 +0,0 @@
-def evaluate(action, state):
-    score = 0
-    if action == "dispatch_ambulance" and state.scene["injuries"] == "yes":
-        score += 10
-    if action == "wait":
-        score -= 5
-    return score
